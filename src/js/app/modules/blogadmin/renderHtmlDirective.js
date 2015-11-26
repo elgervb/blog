599d79f85ddf323a13659d49946556ba686ac33a
Elger van Boxtel <elgervb@gmail.com>
2015-11-26 17:01:31 +0100

    add HTML parser

---
+++ src/js/app/modules/blogadmin/renderHtmlDirective.js
@@ -0,0 +1,24 @@
+/* global angular */
+angular.module('blog').directive('renderTemplate', ($compile) => {
+  return {
+    link: (scope, element, attrs) => {
+      scope.$watch(
+        (innerScope) => {
+            // watch the 'renderTemplate' expression for changes
+          return innerScope.$eval(attrs.renderTemplate);
+        },
+        (value) => {
+          // when the 'renderTemplate' expression changes
+          // assign it into the current DOM
+          element.html(value);
+
+          // compile the new DOM and link it to the current
+          // scope.
+          // NOTE: we only compile .childNodes so that
+          // we don't get into infinite loop compiling ourselves
+          $compile(element.contents())(scope);
+        }
+      );
+    }
+  };
+});
