3fd4ea7c526803301b554931eb47259f80e7534a
Elger van Boxtel <elgervb@gmail.com>
2015-12-01 20:21:14 +0100

    controller for guid example

--- src/js/app/modules/blog.shared/examples/guidController.js
+++ src/js/app/modules/blog.shared/examples/guidController.js
@@ -1,6 +1,16 @@
 /* global angular */
 angular.module('blog.shared').controller('guidController', ($scope) => {
+  
+  
+  let createGUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      let r = Math.random() * 16 | 0;
+      let v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  };
+  
   $scope.generate = () => {
-    $scope.guid = 'asdf';
+    $scope.guid = createGUID();
   };
 });
