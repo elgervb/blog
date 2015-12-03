7f620f22118e751d81242817fcc9ce861daf23f0
Elger van Boxtel <elgervb@gmail.com>
2015-12-03 15:29:29 +0100

    be able to reset the form to it's original state

--- src/js/app/modules/blog.admin/directive/postForm.directive.js
+++ src/js/app/modules/blog.admin/directive/postForm.directive.js
@@ -8,6 +8,41 @@ angular.module('blog.admin').directive('postForm', () => {
     scope: {
       post: '=',
       submit: '='
+    },
+    link: (scope) => {
+      let original;
+      
+      // watch for async changes
+      let unwatch = scope.$watch(() => {
+        return scope.post;
+      }, (value) => {
+        // run only when value is set and original not
+        if (value && !original) {
+          original = angular.copy(value);
+          // set the original value only once, unwatch this when set
+          unwatch();
+        }
+      });
+      
+      scope.cancel = () => {
+        scope.post = angular.copy(original); // must copy this, as otherwise we will get a pointer to original an thus updating it
+        scope.form.$setPristine();
+      };
+      
+      /**
+       * Submit the form
+       * @param {object} post the post object
+       * @param {Form} form the angular form object
+       * @return {void}
+       */
+      scope.submitForm = (post, form) => {
+        // update the original value
+        original = angular.copy(post);
+        // calling the provided submit callback
+        if (typeof scope.submit === 'function') {
+          scope.submit(post, form);
+        }
+      };
     }
   };
 });
