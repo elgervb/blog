ffb92c681f6016abecf332844bd5af352434138b
Elger van Boxtel <elgervb@gmail.com>
2015-12-03 16:46:41 +0100

    Close distractionless mode with the escape key

--- src/js/app/modules/blog.admin/directive/distractionless.directive.js
+++ src/js/app/modules/blog.admin/directive/distractionless.directive.js
@@ -6,12 +6,30 @@ angular.module('blog.admin').directive('distractionless', ($document) => {
     templateUrl: 'modules/blog.admin/directive/distractionless.directive.html',
     scope: {},
     link: (scope, element) => {
+      let ESCAPE_KEY = 27;
+      
       element.addClass('distractionless');
       
       scope.toggle = () => {
         element.toggleClass('enabled');
         $document.find('body').toggleClass('distractionless-noscroll');
       };
+      
+      let close = (e) => {
+        if (ESCAPE_KEY === e.keyCode) {
+          
+          element.removeClass('enabled');
+          $document.find('body').removeClass('distractionless-noscroll');
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      };
+      
+      $document.on('keyup', close);
+      
+      scope.$on('$destroy', () => {
+        $document.off('keyup', close);
+      });
     }
   };
 });
