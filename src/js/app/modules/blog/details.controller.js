86894083c65cf1d7b6195dab90f6c92f64e3e891
Elger van Boxtel <elgervb@gmail.com>
2015-12-08 23:12:26 +0100

    #3 Next or previous post can result in a 404
    Fixed by handle the routing inside the details controller. If there is
    no next (or previous) then return to the posts list
    fixed #3

--- src/js/app/modules/blog/details.controller.js
+++ src/js/app/modules/blog/details.controller.js
@@ -10,5 +10,35 @@ angular.module('blog').controller('details', ($scope, $log, $state, $stateParams
     $log.error(res);
     $state.go('main.list');
   });
+  
+  /**
+   * Go to the next post, based on the current post
+   * 
+   * @param {object} post the current post
+   * 
+   * @return {undefined}
+   */
+  $scope.goNext = (post) => {
+    if (post.next) {
+      $state.go('main.details', {postId: post.next});
+    } else {
+      $state.go('main.list');
+    }
+  }
+  
+  /**
+   * Go to the previous post, based on the current post
+   * 
+   * @param {object} post the current post
+   * 
+   * @return {undefined}
+   */
+  $scope.goPrevious = (post) => {
+    if (post.previous) {
+      $state.go('main.details', {postId: post.previous});
+    } else {
+      $state.go('main.list');
+    }
+  }
 
 });
