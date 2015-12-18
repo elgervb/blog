3817e4091d7ede475a09fde824e880a3c71f900c
Elger van Boxtel <elgervb@gmail.com>
2015-12-18 08:53:47 +0100

    added HttpStatus 204 no content when requesting a post that does not exists

--- server/api/index.php
+++ server/api/index.php
@@ -6,12 +6,15 @@ use handler\Handlers;
 use handler\json\JsonHandler;
 use handler\http\HttpStatusHandler;
 use handler\json\Json;
+use handler\http\HttpStatus;
+use http\HttpSession;
 
 include __DIR__ . '/../vendor/autoload.php';
 include 'Model_Post.php';
 
 // setup database
-R::setup( 'sqlite:./db/dbfile.db' );
+R::setup('sqlite:./db/dbfile.db');
+// all dates in UTC timezone
 date_default_timezone_set("UTC");
 ini_set('date.timezone', 'UTC');
 
@@ -22,58 +25,71 @@ $handlers->add(new HttpStatusHandler());
 
 /**
  * Fetch all posts
+ *
+ * @return Json array with all posts
  */
-$router->route('posts-list', '/posts', function() {
+$router->route('posts-list', '/posts', function ()
+{
     $result = [];
     $posts = R::find('post', ' isActive = 1 ORDER BY created DESC');
     /* @var $post RedBeanPHP\OODBBean */
-    foreach($posts as $post) {
+    foreach ($posts as $post) {
         $result[] = $post->export();
     }
     return new Json($result);
 })
-
 /**
  * Fetch all drafts
+ *
+ * @return Json array with all drafts
  */
-->route('drafts-list', '/posts/drafts', function() {
+->route('drafts-list', '/posts/drafts', function ()
+{
     $result = [];
     $drafts = R::find('post', ' isActive != 1 ORDER BY created DESC');
     /* @var $post RedBeanPHP\OODBBean */
-    foreach($drafts as $draft) {
+    foreach ($drafts as $draft) {
         $result[] = $draft->export();
     }
     return new Json($result);
 })
-
 /**
  * Fetch a single post
+ *
+ * @return Json post object | HttpStatus 404 when no posts found
  */
-->route('post', '/posts/:id', function($id) {
-    $post = R::load( 'post', $id );
+->route('post', '/posts/:id', function ($id)
+{
+    $post = R::load('post', $id);
     
     if ($post->id) {
-        $next = R::findOne('post', ' id > ? AND isactive = 1 ORDER BY id ASC', [$post->id]);
+        $next = R::findOne('post', ' id > ? AND isactive = 1 ORDER BY id ASC', [
+            $post->id
+        ]);
         if ($next) {
             $post->next = $next->id;
         }
-        $previous = R::findOne('post', ' id < ? AND isactive = 1 ORDER BY id DESC', [$post->id]);
+        
+        $previous = R::findOne('post', ' id < ? AND isactive = 1 ORDER BY id DESC', [
+            $post->id
+        ]);
         if ($previous) {
             $post->previous = $previous->id;
         }
     } else {
         // no post found... give a 404
-        header("HTTP/1.0 404 Not Found");
-        return;
+        return new HttpStatus(HttpStatus::STATUS_404_NOT_FOUND);
     }
     
     return new Json($post->export());
 })
-
 /**
  * Add a single post
+ * 
+ * @return Json the newly added post | HttpStatus 204 no content when no data available on http post
  */
-->route('add-post', '/posts', function() {
+->route('add-post', '/posts', function ()
+{
     $post = R::dispense('post');
     
     $request = HttpContext::get()->getRequest();
@@ -86,20 +102,25 @@ $router->route('posts-list', '/posts', function() {
         $post->created = R::isoDateTime(time());
         
         R::store($post);
+    } else {
+        return new HttpStatus(HttpStatus::STATUS_204_NO_CONTENT);
     }
     
     return new Json($post->export());
 }, 'POST')
 /**
  * Edit a single post
+ * 
+ * @return Json post object | HttpStatus 
  */
-->route('edit-post', '/posts/:id', function($id) {
-    $post = R::load( 'post', $id );
+->route('edit-post', '/posts/:id', function ($id)
+{
+    $post = R::load('post', $id);
     
     if ($post->id != $id) {
-        return;
+        return new HttpStatus(HttpStatus::STATUS_204_NO_CONTENT);
     }
-
+    
     $request = HttpContext::get()->getRequest();
     if ($request->hasPost('post')) {
         $post->title = $request->getPost('post', 'title');
@@ -109,10 +130,9 @@ $router->route('posts-list', '/posts', function() {
         $post->controller = $request->getPost('post', 'controller');
         R::store($post);
     }
-
+    
     return new Json($post->export());
 }, 'PUT');
-
 
 $result = $router->match($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 
