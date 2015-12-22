976207d26332a402df99f6e9aea4a4ea997c28bc
Elger van Boxtel <elgervb@gmail.com>
2015-12-22 14:58:20 +0100

    restructured blog folder structure

--- src/js/app/modules/blog/blog.module.js
+++ src/js/app/modules/blog/blog.module.js
@@ -11,16 +11,16 @@ angular.module('blog', ['ngMessages', 'blog.shared'])
   $stateProvider
   .state('main', {
     abstract: true,
-    templateUrl: 'modules/blog/blog.html'
+    templateUrl: 'modules/blog/controllers/blog.html'
   })
   .state('main.list', {
     url: '/list',
-    templateUrl: 'modules/blog/list.html',
+    templateUrl: 'modules/blog/controllers/list.html',
     controller: 'list'
   })
   .state('main.details', {
     url: '/post/:postId',
-    templateUrl: 'modules/blog/details.html',
+    templateUrl: 'modules/blog/controllers/details.html',
     controller: 'details'
   });
 });
